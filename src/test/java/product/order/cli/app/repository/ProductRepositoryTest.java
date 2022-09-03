02a07960e25240121c68806e30157ea48247d154
seonje <leeseonje9323@gmail.com>
2022-09-03 19:55:27 +0900

    dataJpaTest

---
+++ src/test/java/product/order/cli/app/repository/ProductRepositoryTest.java
@@ -0,0 +1,38 @@
+package product.order.cli.app.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import product.order.cli.app.domain.Product;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+class ProductRepositoryTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @BeforeEach
+    void init() {
+        for (long i = 0L; i < 20L; i++) {
+            int stockQuantity = (int) (Math.random() * 100);
+            Product product = Product.of(i, "product" + i, "10000", stockQuantity);
+            productRepository.save(product);
+        }
+    }
+
+    @Test
+    void findByNumbers() {
+        List<Long> ids = List.of(1L, 2L, 3L, 4L, 5L);
+
+        List<Product> products = productRepository.findByNumbers(ids);
+
+        assertThat(products.size()).isEqualTo(5);
+    }
+
+}
